b04a2ba875df11679294e4c1f6459ab4ca4ae1e7
三島智貴 <141534298+mametaro99@users.noreply.github.com>
2024-12-17 02:18:01 +0000

    Userモデルのgender(性別)をenumを使って定義。また、Gemfileのenum_helpをインストールして、ja.ymlに定義した各性別の日本語の定義を呼び出せるようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  enum gender: {other: 0 , male: 1, female: 2}
   with_options presence: true do
     validates :name
     validates :age
