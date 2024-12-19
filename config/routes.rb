46f55181a8353883284d60c36b8f94f4130fe95c
use-hunks <110585354+use-hunks@users.noreply.github.com>
2024-12-19 14:32:10 +0000

    新規登録とログイン画面を作成（CSSまだ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :users 
+  devise_for :users , controllers: {
+  registrations: 'user/registrations',
+  sessions: 'user/sessions'
+}
   root "home#index"  
 end
