62a2257b40e6199075d073511f6fcb0eebeb9c3b
2002ryosuke <125632218+2002ryosuke@users.noreply.github.com>
2024-12-20 14:55:16 +0000

    イベントのカラム名と型変更

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :subgenre
   belongs_to :user
-  belongs_to :event
+  # belongs_to :event
+  belongs_to :matched_event, class_name: 'Event', foreign_key: 'matched_id', optional: true
+  has_many :matching_events, class_name: 'Event', foreign_key: 'matched_id'
+
+
   has_many :matching_event_groups
 end
