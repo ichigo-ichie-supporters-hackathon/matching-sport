4405e3b3a3e409a9b9a6fdee16e61076c35144ef
use-hunks <110585354+use-hunks@users.noreply.github.com>
2024-12-21 08:37:53 +0000

    簡易的なマッチングの実装

--- app/controllers/user/event_controller.rb
+++ app/controllers/user/event_controller.rb
@@ -29,6 +29,8 @@ class User::EventController < ApplicationController
     end
   
     if @event.save
+      # マッチングのアルゴリズムを実装する
+      match_event(@event)
       redirect_to user_event_path(@event), notice: 'イベントの登録に成功しました。', turbo: false
     else
       load_genres_and_subgenres
@@ -60,6 +62,38 @@ class User::EventController < ApplicationController
     params.require(:event).permit(
       :address, :latitude, :longitude, :start_time, :end_time, :comment, :people_count, :position,:genre_id, :subgenre_id, :user_id, :unmetched_gender, :unmatched_age_min, :unmatched_age_max, :is_matched, :is_accepted
     )
+  end
+
+  # マッチングのアルゴリズムの内容
+  def match_event(new_event)
+    # すでにマッチングしていないEventかつ、今の時間よりも開始時間が後のものかつ、自分以外のユーザーが作成したイベントを抽出する
+    matching_events = Event.where(matched_id: nil).where("start_time > ?", Time.current).where.not(user_id: current_user.id)
+    # サブジャンルが一致するイベントを優先
+    matching_events = matching_events.where(genre_id: new_event.genre_id).where(sub_genre_id: new_event.sub_genre_id)
+    matching_events = mathing_events.where(genre_id: new_event.genre_id) if matching_events.empty?
+
+    # 時間でまず絞り込む
+    matching_events = matching_events.where("start_time <= ? AND end_time >= ?", new_event.end_time, new_event.start_time)
+
+    # 位置情報でさらに絞り込む（Geocoderを利用）
+    matching_events = matching_events.near([new_event.latitude, new_event.longitude], 10, units: :km)
+
+    # 残りのイベントをRuby側で絞り込む（もし必要なら）
+    matching_events = matching_events.select do |event|
+      is_time_overlap = event.start_time <= new_event.end_time && new_event.start_time <= event.end_time
+      is_time_overlap
+    end
+    # 条件に合致するか
+     
+    # 最も近いイベントを選択する
+    best_match_event = mathing.events.min_by(&:start_time)
+    if best_match_event
+      new_event.update(matched_id: new_event.event_id)
+      best_match_event.update(matched_event: new_event.event_id)
+    else 
+      # マッチングするイベントが見つからなかった場合
+      flash[:notice] = "マッチングするまでお待ちください"
+    end
   end
 
 end
